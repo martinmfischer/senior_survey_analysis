3c3d6bfe283057e6a3f019f4091131cb07e5dfa3
Luise Anter <luise.anter@ifkw.lmu.de>
2025-10-07 16:29:24 +0200

    Also recoded frequency variables (engagement + platform use, now 1 = never, 8 = several times per day)

--- 02_Scripts/03_Descriptive_Analysis.R
+++ 02_Scripts/03_Descriptive_Analysis.R
@@ -55,6 +55,18 @@ data_rev <- clean_data %>% reverse_scale(implicit_1,
                              lower_end = 5,
                              upper_end = 1)
 
+data_rev <- data_rev %>% reverse_scale(engagement_1,
+                                       engagement_2,
+                                       engagement_3,
+                                       engagement_4,
+                                       facebook_usage,
+                                       instagram_usage,
+                                       tiktok_usage,
+                                       x_usage,
+                                       youtube_usage,
+                                       lower_end = 8,
+                                       upper_end = 1)
+
 # --- Recode curation-items -------------------------------------------------
 # Note: Checkbox-Scale, 'selected' was coded inconsistently (1 - 6)
 
@@ -83,55 +95,114 @@ data_rev <- data_rev %>%
 data_idx <- data_rev %>%
   # Perceived usefulness
   add_index(idx_implicit,
-            implicit_1_rev, implicit_2, implicit_3_rev, implicit_4,
+            implicit_1_rev, 
+            implicit_2, 
+            implicit_3_rev,
+            implicit_4,
             cast.numeric = TRUE) %>%
   add_index(idx_explicit,
-            explicit_1_rev, explicit_2_rev, explicit_3_rev, explicit_4,
+            explicit_1_rev, 
+            explicit_2_rev, 
+            explicit_3_rev, 
+            explicit_4,
             cast.numeric = TRUE) %>%
   add_index(idx_incidentalness,
-            incidentalness_1_rev, incidentalness_2, incidentalness_3_rev, 
-            incidentalness_4_rev, incidentalness_5,
+            incidentalness_1_rev, 
+            incidentalness_2,
+            incidentalness_3_rev, 
+            incidentalness_4_rev,
+            incidentalness_5,
             cast.numeric = TRUE) %>%
   add_index(idx_sociality,
-            sociality_1_rev, sociality_2, sociality_3_rev, sociality_4_rev, 
-            sociality_5, cast.numeric = TRUE) %>%
+            sociality_1_rev, 
+            sociality_2, 
+            sociality_3_rev, 
+            sociality_4_rev, 
+            sociality_5, 
+            cast.numeric = TRUE) %>%
   # Practices
   add_index(idx_snacking, 
-            snacking_1_rev, snacking_2, snacking_3_rev, snacking_4,
-            snacking_5_rev, snacking_6, snacking_7, snacking_8_rev,
+            snacking_1_rev, 
+            snacking_2, 
+            snacking_3_rev, 
+            snacking_4,
+            snacking_5_rev, 
+            snacking_6, 
+            snacking_7,
+            snacking_8_rev,
             cast.numeric = TRUE) %>%
   add_index(idx_engagement, 
-            engagement_1, engagement_2, engagement_3, engagement_4,
+            engagement_1_rev, 
+            engagement_2_rev, 
+            engagement_3_rev,
+            engagement_4_rev,
             cast.numeric = TRUE) %>%
   add_index(idx_curation, 
-            curation_1_bin, curation_2_bin, curation_3_bin, curation_4_bin,
-            curation_5_bin, curation_6_bin,
+            curation_1_bin, 
+            curation_2_bin, 
+            curation_3_bin,
+            curation_4_bin,
+            curation_5_bin, 
+            curation_6_bin,
             cast.numeric = TRUE) %>%
   # Information use (2-item indices)
-  add_index(idx_info_undirected, undirected_news_rev, undirected_life_rev,           
+  add_index(idx_info_undirected, 
+            undirected_news_rev,
+            undirected_life_rev,           
             cast.numeric = TRUE) %>%
-  add_index(idx_info_topic,      topic_interests_rev, topic_hobbies_rev,             
+  add_index(idx_info_topic,     
+            topic_interests_rev,
+            topic_hobbies_rev,             
             cast.numeric = TRUE) %>%
-  add_index(idx_info_problem,    problem_specific_need_rev, problem_solving_rev,           
+  add_index(idx_info_problem,    
+            problem_specific_need_rev, 
+            problem_solving_rev,           
             cast.numeric = TRUE) %>%
-  add_index(idx_info_group,      group_close_rev, group_extended_rev,            
+  add_index(idx_info_group,      
+            group_close_rev, 
+            group_extended_rev,            
             cast.numeric = TRUE)
 
-# --- Reliability (psych) ---------------------------------------------------
+# --- Reliability -----------------------------------------------------------
 
 # ωh (drop-one only if ωh < .70) for perceptions and practices
 pp_scales <- list(
-  implicit       = c("implicit_1_rev","implicit_2","implicit_3_rev","implicit_4"),
-  explicit       = c("explicit_1_rev","explicit_2_rev","explicit_3_rev","explicit_4"),
-  incidentalness = c("incidentalness_1_rev","incidentalness_2","incidentalness_3_rev",
-                     "incidentalness_4_rev","incidentalness_5"),
-  sociality      = c("sociality_1_rev","sociality_2","sociality_3_rev","sociality_4_rev",
+  implicit       = c("implicit_1_rev",
+                     "implicit_2",
+                     "implicit_3_rev",
+                     "implicit_4"),
+  explicit       = c("explicit_1_rev",
+                     "explicit_2_rev",
+                     "explicit_3_rev",
+                     "explicit_4"),
+  incidentalness = c("incidentalness_1_rev",
+                     "incidentalness_2",
+                     "incidentalness_3_rev",
+                     "incidentalness_4_rev",
+                     "incidentalness_5"),
+  sociality      = c("sociality_1_rev",
+                     "sociality_2",
+                     "sociality_3_rev",
+                     "sociality_4_rev",
                      "sociality_5"),
-  snacking       = c("snacking_1_rev","snacking_2","snacking_3_rev","snacking_4",
-                     "snacking_5_rev","snacking_6","snacking_7","snacking_8_rev"),
-  engagement     = c("engagement_1","engagement_2","engagement_3","engagement_4"),
-  curation       = c("curation_1_bin","curation_2_bin","curation_3_bin",
-                     "curation_4_bin","curation_5_bin", "curation_6_bin")
+  snacking       = c("snacking_1_rev",
+                     "snacking_2",
+                     "snacking_3_rev",
+                     "snacking_4",
+                     "snacking_5_rev",
+                     "snacking_6",
+                     "snacking_7",
+                     "snacking_8_rev"),
+  engagement     = c("engagement_1_rev",
+                     "engagement_2_rev",
+                     "engagement_3_rev",
+                     "engagement_4_rev"),
+  curation       = c("curation_1_bin",
+                     "curation_2_bin",
+                     "curation_3_bin",
+                     "curation_4_bin",
+                     "curation_5_bin", 
+                     "curation_6_bin")
 )
 
 omega_results <- imap_dfr(pp_scales, ~ {
@@ -230,21 +301,28 @@ tidycomm::tab_frequencies(data_idx, youtube_usage)
 # Social media use: questionnaire platform
 tidycomm::tab_frequencies(data_idx, platform_helper)
 
-# Social media use: platforms used
-platform_vars <- c("facebook_usage","instagram_usage","x_usage","tiktok_usage",
-                   "youtube_usage")
+# Social media use: platforms used 
+# Binary, never (0) vs. at least less than once a month (1)
+
+platform_vars <- c("facebook_usage_rev",
+                   "instagram_usage_rev",
+                   "x_usage_rev",
+                   "tiktok_usage_rev",
+                   "youtube_usage_rev")
 
 data_idx <- data_idx %>%
   mutate(across(all_of(platform_vars),
-                ~ if_else(between(to_num(.), 1, 7), 1L, 0L, missing = 0L),
+                ~ if_else(between(to_num(.), 2, 8), 1L, 0L, missing = 0L),
                 .names = "{.col}_overall_use"))
+
+platform_use_vars  <- paste0(platform_vars, "_overall_use")
 
 platforms_used <- data_idx %>%
   mutate(id = row_number()) %>%
-  select(id, ends_with("_use")) %>%
+  select(id, all_of(platform_use_vars)) %>%
   pivot_longer(-id, names_to = "platform", values_to = "use") %>%
   filter(use == 1L) %>%
-  mutate(platform = sub("_usage_use$", "", platform)) %>%
+  mutate(platform = sub("_usage_rev_overall_use$", "", platform)) %>%
   group_by(id) %>%
   summarise(
     n_platforms_used = n(),
@@ -268,29 +346,30 @@ tidycomm::describe(data_idx, idx_incidentalness)
 
 tidycomm::describe(data_idx, idx_sociality)
 
-# Explorative Analysis: Differences between platforms + users
-
 # --- RQ2: Descriptive Analysis of Practices --------------------------------
 
 # Engagement: Mean index & frequencies of individual engagement practices
 tidycomm::describe(data_idx, idx_engagement)
 
-engagement_vars <- c("engagement_1","engagement_2","engagement_3","engagement_4")
+engagement_vars <- c("engagement_1_rev","engagement_2_rev",
+                     "engagement_3_rev","engagement_4_rev")
 
 data_idx <- data_idx %>%
   mutate(across(
     all_of(engagement_vars),
     ~ case_when(
-      between(as.integer(.), 1, 4) ~ "often",
-      between(as.integer(.), 5, 7) ~ "rarely",
-      as.integer(.) == 8           ~ "never",
+      between(as.integer(.), 5, 8) ~ "often",
+      between(as.integer(.), 2, 4) ~ "rarely",
+      as.integer(.) == 1           ~ "never",
       TRUE                         ~ NA_character_
     ),
     .names = "{.col}_overall_use"
   ))
 
-engagement_overall_vars <- c("engagement_1_overall_use","engagement_2_overall_use",
-                     "engagement_3_overall_use","engagement_4_overall_use")
+engagement_overall_vars <- c("engagement_1_rev_overall_use",
+                             "engagement_2_rev_overall_use",
+                             "engagement_3_rev_overall_use",
+                             "engagement_4_rev_overall_use")
 
 engagement_frequencies <- data_idx %>%
   select(all_of(engagement_overall_vars)) %>%
